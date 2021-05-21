9a51867298c2b210d18b608e9a8a9e738a1f60f8
SquirrelyGirl24 <donna.krutka@outlook.com>
2021-05-21 17:31:57 -0600

    first take with comments

--- src/com/skilldistillery/app/FoodTruck.java
+++ src/com/skilldistillery/app/FoodTruck.java
@@ -1,5 +1,56 @@
 package com.skilldistillery.app;
 
 public class FoodTruck {
+	private String truckName;
+	private String foodType;
+	private double rating;
+	private int truckID;
+
+	public FoodTruck() {
+
+	}
+
+	public FoodTruck(String truckName, String foodType, double rating, int truckID) {
+		this.truckName = truckName;
+		this.foodType = foodType;
+		this.rating = rating;
+		this.truckID = truckID;
+	}
+
+	public FoodTruck(String truckName, String foodType, double rating, int truckID) {
+		this(truckName, foodType, rating, 0.0);
+	}
+
+	public String getTruckName() {
+		return truckName;
+	}
+
+	public void setTruckName(String truckName) {
+		this.truckName = truckName;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
+	public int getTruckID() {
+		return truckID;
+	}
+
+	public void setTruckID(int truckID) {
+		this.truckID = truckID;
+	}
 
 }
--- src/com/skilldistillery/app/FoodTruckApp.java
+++ src/com/skilldistillery/app/FoodTruckApp.java
@@ -1,10 +1,32 @@
 package com.skilldistillery.app;
 
+import java.util.Scanner;
+
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		FoodTruckApp foodTruckApp = new FoodTruckApp();
+		
 	}
+	
+	
 
 }
+// Welcome to the Food Truck App!
+// You can enter up to 5 different food trucks to rate.
+// 1. Enter the name of the truck: 
+// 2. Enter the type of food they serve:
+// 3. Enter a rating of 1-5 for this food truck:
+// 4. Do you have another food truck to rate? Y or N
+// *** input ends if N is selected, take this as quit and end the program continues
+
+// Please choose one of the following:
+// 1. List all existing food trucks.
+// 2. See the average rating of food trucks.
+// 3. Display the highest-rated food truck.
+// 4. Quit the program.
+// *** run this menu till the user chooses quit
+// when quit is chosen, display Thank you for using the Food Truck App, have a great day!
+// *** if <5 trucks were entered only display those entered, no null
+
